648e53cae2adf815f7ee2842e37b3ec9a536f9fd
VovkAndriy <andriy.vovk.work@gmail.com>
2024-10-08 14:32:57 +0300

    implement endpoint in fastapi for getting json from requested text

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from json_creator import create_json_from_text
+from models import TextRequest
+
+
+app = FastAPI()
+
+
+@app.post("/get-json/")
+async def get_json_from_text(request: TextRequest):
+    """
+    Receive chinese text and return a json response.
+    """
+    return await create_json_from_text(request.text)
